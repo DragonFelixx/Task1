d327e0474bbccbb28a08900feffd3d6687f19ccd
Velichko Ilya <fantsen@yandex.ru>
2020-10-09 00:18:52 +0300

    pf2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,23 +5,21 @@ import java.util.Scanner;
 public class Main
 {
     public static void main(String[] args) {
-        double deposit, percent, date, interestReceived, result;
+        double deposit = readDouble("кол-во вложенных денег:");
+        double percent = readDouble("процент вклада(проценты в месяц):");
+        double date = readDouble("срок(в месяцах):");
 
-        deposit = readDouble("кол-во вложенных денег:");
-        percent = readDouble("процент вклада(проценты в месяц):");
-        date = readDouble("срок(в месяцах):");
-
-        interestReceived = interestReceived(percent);
-        result = result(deposit, date, interestReceived);
+        double interestReceived = FindinterestReceived(percent);
+        double result = FindEarning(deposit, date, interestReceived);
 
         System.out.println("Доход = " + result);
     }
 
-    private static double interestReceived(double percent) {
+    private static double FindinterestReceived(double percent) {
         return  1 + percent/100;
     }
 
-    private static double result(double deposit, double date, double interestReceived) {
+    private static double FindEarning(double deposit, double date, double interestReceived) {
         return deposit * Math.pow(interestReceived, date) - deposit;
     }
 
